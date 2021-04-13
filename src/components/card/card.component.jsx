eb03bbe48397a866cc42c51a161f91549ac6a16b
Michel <michel.sunsin@gmail.com>
2021-04-13 11:30:42 -0300

    initial commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import './card.styles.css';
+
+export const Card = ({ monster }) => (
+  <div className='card-container'>
+    <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt='monster' />
+    <h1>{monster.name}</h1>
+    <p>{monster.email}</p>
+  </div>
+);
